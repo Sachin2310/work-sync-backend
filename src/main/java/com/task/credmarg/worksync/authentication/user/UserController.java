df237ab5e5bdd8bcb7069d07020ab9677a3bde57
Sachin2310 <warlord2op@gmail.com>
2024-07-26 10:19:56 +0530

    feat: employee service update

--- src/main/java/com/task/credmarg/worksync/authentication/user/UserController.java
+++ src/main/java/com/task/credmarg/worksync/authentication/user/UserController.java
@@ -2,12 +2,14 @@ package com.task.credmarg.worksync.authentication.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
--- src/main/java/com/task/credmarg/worksync/employee/EmployeeController.java
+++ src/main/java/com/task/credmarg/worksync/employee/EmployeeController.java
@@ -3,6 +3,7 @@ package com.task.credmarg.worksync.employee;
 import com.task.credmarg.worksync.employee.models.EmployeeDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/employee")
 @RequiredArgsConstructor
