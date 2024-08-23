d5380c18dd2a48d6b989c4b19e45a3f9df3348c9
Sachin2310 <warlord2op@gmail.com>
2024-08-23 20:12:16 +0530

    refactor: in-memory user management changes

--- src/main/java/com/task/credmarg/worksync/authentication/user/service/InMemoryUserManagementService.java
+++ src/main/java/com/task/credmarg/worksync/authentication/user/service/InMemoryUserManagementService.java
@@ -1,39 +1,45 @@
-// package com.task.credmarg.worksync.authentication.user.service;
-//
-// import com.task.credmarg.worksync.authentication.user.User;
-// import com.task.credmarg.worksync.authentication.user.controller.UserDTO;
-// import java.util.HashMap;
-// import java.util.Map;
-// import org.springframework.stereotype.Service;
-//
-// @Service
-// public class InMemoryUserManagementService implements UserManagementService {
-//    Map<String, User> users = new HashMap<>();
-//
-//    {
-//        users.put("admin", new User(100, "admin", "admin", "admin", null));
-//    }
-//
-//    @Override
-//    public UserDTO addUser(UserDTO userDTO) {
-//        users.put(userDTO.getEmail(), mapUserDtoToUserDetails(userDTO));
-//        return userDTO;
-//    }
-//
-//    private User mapUserDtoToUserDetails(UserDTO userDTO) {
-//        return User.builder()
-//                .name(userDTO.getUserName())
-//                .email(userDTO.getEmail())
-//                .password(userDTO.getPassword())
-//                .build();
-//    }
-//
-//    @Override
-//    public boolean verifyUser(UserDTO userDTO) {
-//        if (!users.containsKey(userDTO.getEmail())) {
-//            return false;
-//        }
-//        return users.get(userDTO.getEmail()).getUserName().equals(userDTO.getUserName())
-//                && users.get(userDTO.getEmail()).getPassword().equals(userDTO.getPassword());
-//    }
-// }
+package com.task.credmarg.worksync.authentication.user.service;
+
+import com.task.credmarg.worksync.authentication.user.User;
+import com.task.credmarg.worksync.authentication.user.controller.SignUpRequest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InMemoryUserManagementService implements UserManagementService {
+    private final UserInformationMapper userInformationMapper;
+    Map<String, User> users = new HashMap<>();
+
+    {
+        users.put("admin", new User(100, "admin", "admin", "admin", null));
+    }
+
+    @Override
+    public SignUpRequest addUser(SignUpRequest signUpRequest) {
+        var userDetails = userInformationMapper.toUserDetails(signUpRequest);
+        users.put(signUpRequest.email(), userDetails);
+        return signUpRequest;
+    }
+
+    @Override
+    public boolean verifyUser(SignUpRequest signUpRequest) {
+        return users.keySet().stream()
+                .filter(email -> email.equals(signUpRequest.email()))
+                .map((email) -> matchUserDetails(email, signUpRequest))
+                .findFirst()
+                .orElse(false);
+    }
+
+    private boolean matchUserDetails(String email, SignUpRequest signUpRequest) {
+        return users.get(email).getPassword().equals(signUpRequest.password());
+    }
+
+    @Override
+    public boolean isValidUser(String userEmail) {
+        return Optional.ofNullable(users.get(userEmail)).isPresent();
+    }
+}
